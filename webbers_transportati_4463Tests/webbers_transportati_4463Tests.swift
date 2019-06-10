79f9ab269e7c3163a026aa3b70565caa1e4aa67d
Crowdbotics <team@crowdbotics.com>
2019-06-10 01:21:28 +0000

    initial commit

---
+++ webbers_transportati_4463Tests/webbers_transportati_4463Tests.swift
@@ -0,0 +1,34 @@
+//
+//  webbers_transportati_4463Tests.swift
+//  webbers_transportati_4463Tests
+//
+//  Created by Dan on 10/23/18.
+//  Copyright © 2018 Crowdbotics. All rights reserved.
+//
+
+import XCTest
+@testable import webbers_transportati_4463
+
+class webbers_transportati_4463Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
